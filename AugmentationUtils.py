c336bdd4f4a24968cb99696079d3c2df12de08f6
Hawk-chan <30329240+natalia0911@users.noreply.github.com>
2022-08-10 20:01:50 -0600

    Transformations updated

--- AugmentationUtils.py
+++ AugmentationUtils.py
@@ -23,7 +23,7 @@ def plot_examples(images, bboxes=None):
     columns = 4
     rows = 5
 
-    for i in range(1, len(images)):
+    for i in range(1, len(images)+1):
         if bboxes is not None:
             img = visualize_bbox(images[i - 1], bboxes[i - 1])
         else:
@@ -49,7 +49,10 @@ def tensor_to_PILImage(imgTensor) -> Image:
     # define a transform to convert a tensor to PIL image
     transform = T.ToPILImage()
     return transform(imgTensor)
-    
+
+def resizeTensor(tensor) -> torch.Tensor:
+    return tensor.resize_(tensor.shape[2], tensor.shape[1], tensor.shape[0])
+
 ###########################################################
 
 
@@ -58,47 +61,33 @@ def augment_image(image, how_many, probability):
     Augmentates the received image and returns it 
     '''
     #Image dimension
-    ig = image
-    print("ANTES DE AUMENTAR",ig.dtype)
     image = tensor_to_PILImage(image)
     w = image.width
     h = image.height
-    print(w,h)
-    
     #Compose class receives a list with augmentations and it returns the transformation function
     transform = A.Compose(
         [
-            A.Resize(width=w, height=h),
-            A.RandomCrop(width=w, height=h), #recortar pero que sea menor a la imagen este si
-            #A.RandomBrightnessContrast(p=probability), #con 20% de probabilidad cambiara el brillo y contraste
-            A.Rotate(limit=40, p=probability),# border_mode=cv2.BORDER_CONSTANT, #con 90% de probabilidad se va a rotar
-            A.HorizontalFlip(p=probability), #voltear horizontal con probabilidad de 50%  NO
-            A.VerticalFlip(p=probability), #voltear vertical con probabilidad de 10%      NO
-            #No afecta y brillo tampoco.
-            #A.RGBShift(r_shift_limit=25, g_shift_limit=25, b_shift_limit=25, p=probability), #Cambia aleatoriamente los valores para cada canal de la imagen RGB de entrada.
-            #Se escoge uno de los siguientes con probabilidad del 10%
+            #It Chooses only one of the transformations
             A.OneOf([
-                A.Blur(blur_limit=3, p=probability), #Desenfocar con tamaño de nucleo a desenforcar y probabilidad
-                #A.ColorJitter(p=0.5), #Fluctuar color (El brillo, el contraste y la saturación)
+                A.RandomCrop(width=w-5, height=h-5, p=probability),
+                A.Resize(width=w-5, height=h+5, p=probability),
+                A.Rotate(limit=20, p=probability),
+                A.Blur(blur_limit=5, p=probability), 
+                A.OpticalDistortion(p=probability),
+                A.GaussNoise (var_limit=(10, 300), p=probability),
+                A.Emboss (alpha=(0.2, 0.5), strength=(0.2, 0.7), p=probability),
+                A.PixelDropout (dropout_prob=0.01, drop_value=0, p=probability),
+                A.Solarize (p=probability)
             ], p=probability),
         ]
     )
 
-    
-    #Convert to numpy arry
     image = np.array(image) 
-    images_list = []
+    #The arg is the image to be trasformed
+    augmentations = transform(image=image)
+    #transform returns a dictionary and the image is in the key image
+    augmented_img = augmentations["image"] 
+    augmented_img = numpyArray_to_tensor(augmented_img)
+    augmented_img = resizeTensor(augmented_img)
 
-    for i in range(how_many):
-        #The arg is the image to be trasformed
-        augmentations = transform(image=image)
-        #transform returns a dictionary and the image is in the key image
-        augmented_img = augmentations["image"] 
-        images_list.append(augmented_img) 
-
-    #plot_examples(images_list) 
-    #returns the image in a tensor 
-    imageTensor = numpyArray_to_tensor(images_list[0])
-    imageTensor.resize_(images_list[0].shape[2], images_list[0].shape[1], images_list[0].shape[0])
-
-    return imageTensor
+    return augmented_img
