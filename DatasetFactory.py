5a5435fa1f325bf33402c6cd1c91de2553b83079
Barnum Castillo <barnum2011@live.com>
2023-05-02 17:17:10 -0600

    Fix bug while create folder that already exists

--- DatasetFactory.py
+++ DatasetFactory.py
@@ -1,7 +1,6 @@
 import torchvision
 import torchvision.transforms as transforms
 from DatasetsEnum import DatasetsEnum
-import os
 from pathlib import Path
 
 class DatasetFactory:
@@ -9,14 +8,14 @@ class DatasetFactory:
         self.datasets_path = datasets_path
 
     def create_training_dataset(self, dataset_type:DatasetsEnum):
-        labeled_path = os.path.join(self.datasets_path, dataset_type.value, "all")
+        labeled_path = Path(self.datasets_path, dataset_type.value, "all")
         dataset =  torchvision.datasets.ImageFolder(labeled_path, transform = self.__get_transformation())
         print("Training dataset created")   
         print(dataset)
         return dataset
 
     def create_unlabeled_dataset(self, augmented_dataset_type:DatasetsEnum):
-        datasetPath = os.path.join(self.datasets_path, augmented_dataset_type.value, "all")    
+        datasetPath = Path(self.datasets_path, augmented_dataset_type.value, "all")    
         dataset = torchvision.datasets.ImageFolder(datasetPath, transform = self.__get_transformation())
         print("Unlabeled dataset created")   
         print(dataset)
