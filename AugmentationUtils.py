c5d645b2fa74fe73fc1097121224c4970980f1bf
Barnum Castillo Barquero <barnum2011@live.com>
2024-02-05 20:44:33 -0600

    Fix probability for augmentation, change mechanism to preserve image not augmentated and add Linealtransfer function (#13)
    * Add Linealtransfer function
    
    * Fix probability for augmentation and change mechanism to preserve image not augmentated

--- AugmentationUtils.py
+++ AugmentationUtils.py
@@ -1,49 +1,55 @@
 
 import numpy as np
 import albumentations as A
-from PIL import Image #Image manipulation
+from PIL import Image
 import torch
 import torchvision.transforms as T
-
-
+import random
 def tensor_to_PILImage(imgTensor:torch.Tensor) -> Image:
     # define a transform to convert a tensor to PIL image
     transform = T.ToPILImage()
     return transform(imgTensor)
 
-
-def augment_image(image:torch.Tensor, probability:float):
+def augment_image(tensor_image:torch.Tensor, probability:float):
     '''
     Augmentates the received image and returns it 
     '''
-    #Image dimension
-    image = tensor_to_PILImage(image)
-    w = image.width
-    h = image.height
+    chance = random.uniform(0, 1)
+    augmentate_image = False
+    if(chance < probability):
+        augmentate_image = True
+
+    pil_image = tensor_to_PILImage(tensor_image)
+    array_image = np.array(pil_image) 
+    if(augmentate_image):
+        transformer = create_transformation(pil_image)
+        augmentations = transformer(image=array_image)
+        augmented_image = augmentations["image"]
+        return [augmentate_image, array_image, augmented_image]
+    else:
+        return [augmentate_image, array_image]
+
+def create_transformation(pil_image)->A.Compose:
+    height = pil_image.width
+    width = pil_image.height
+    
     #Compose class receives a list with augmentations and it returns the transformation function
     transform = A.Compose(
         [
             #It Chooses only one of the transformations
             A.OneOf([
-                A.RandomCrop(width=w-5, height=h-5, p=probability),
-                A.Resize(width=w-5, height=h+5, p=probability),
-                A.Rotate(limit=20, p=probability),
-                A.Blur(blur_limit=5, p=probability), 
-                A.OpticalDistortion(p=probability),
-                A.GaussNoise (var_limit=(10, 300), p=probability),
-                A.Emboss (alpha=(0.2, 0.5), strength=(0.2, 0.7), p=probability),
-                A.PixelDropout (dropout_prob=0.01, drop_value=0, p=probability),
-                A.Solarize (p=probability)
-            ], p=probability),
-        ]
+                A.RandomCrop(width=width-5, height=height-5, p=1),
+                A.Resize(width=width-5, height=height+5, p=1),
+                A.Rotate(limit=20, p=1),
+                A.Blur(blur_limit=5, p=1), 
+                A.OpticalDistortion(p=1),
+                A.GaussNoise (var_limit=(10, 300), p=1),
+                A.Emboss (alpha=(0.2, 0.5), strength=(0.2, 0.7), p=1),
+                A.PixelDropout (dropout_prob=0.01, drop_value=0, p=1),
+                A.Solarize (p=1)
+            ])
+        ], p=1
     )
-    should_augmentate = True
-    image = np.array(image) 
-    if(should_augmentate):
-        #The arg is the image to be trasformed
-        augmentations = transform(image=image)
-        #transform returns a dictionary and the image is in the key image
-        augmented_img = augmentations["image"]
-        return augmented_img
-    else:
-        return image
+    
+    return transform
+
